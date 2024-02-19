27b83fe0332fe45b4a826f0cc9adedff622f6cd3
puyex <puyex@RIOMARU-PC>
2024-02-19 10:31:43 +0900

    プロジェクト ファイルを追加します。

---
+++ SimpleDicomViewer/Domain/Models/DataElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 参考文献
+// https://www.jira-net.or.jp/dicom/file/standard/DICOM_PS3.5j_2009_ref.pdf
+
+namespace SimpleDicomViewer.Domain.Models
+{
+    internal class DataElement
+    {
+    }
+}
