be8759432ca6ece66976045674b51537b992bfb1
Puye123 <puye.xiongtai@gmail.com>
2024-02-28 12:55:12 +0900

    Add: Dateを表すクラスを新規作成

---
+++ SimpleDicomViewer/Domain/ValueObjects/VR/DateValue.cs
@@ -0,0 +1,45 @@
+﻿using SimpleDicomViewer.Domain.Exceptions;
+using System;
+using System.Text.RegularExpressions;
+
+namespace SimpleDicomViewer.Domain.ValueObjects.VR
+{
+    /// <summary>
+    /// DA(Date: 日付) : string
+    /// </summary>
+    /// <remarks>
+    /// 書式 YYYYMMDD の文字列；ここで YYYY は年を含み，MM は月を含み，DD は日を含み，グレゴリオ暦の日付として解釈される。 
+    /// 例：”19930822”は 1993 年 8 月 22 日を表す。 
+    /// Todo: 範囲照合問合せには現状非対応であり、必要になったら実装する。
+    /// </remarks>
+    public class DateValue : ValueElement
+    {
+        public DateValue(Tag tag, byte[] value) : base(tag, value, length: 8, isFixedValue: true, valueType: typeof(string))
+        {
+        }
+
+        protected override bool IsValidValue(byte[] value)
+        {
+            if (value.Length != Length) { throw new InvalidDICOMFormatException($"設定可能な値は{Length}バイトのみです。"); }
+
+            try
+            {
+                string valueString = System.Text.Encoding.ASCII.GetString(value);
+                var result = Regex.IsMatch(valueString, @"^[0-9]* *$");
+                if (result)
+                {
+                    return true;
+                }
+                else
+                {
+                    throw new InvalidDICOMFormatException($"設定可能な値は^[0-9]* *$です。設定しようとした値は{valueString}です。");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                return false;
+            }
+        }
+    }
+}
---
+++ SimpleDicomViewerTests/Domain/ValueObjects/VR/DateValueTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleDicomViewer.Domain.Exceptions;
+using SimpleDicomViewer.Domain.ValueObjects.VR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDicomViewer.Domain.ValueObjects.VR.Tests
+{
+    [TestClass()]
+    public class DateValueTests
+    {
+        [TestMethod()]
+        public void 値の長さが正しく設定されているか()
+        {
+            // Arrange
+            var ae = new DateValue(new Tag(0x1234, 0xabcd), new byte[] { 0x30, 0x31, 0x32, 0x30, 0x31, 0x32, 0x30, 0x31 });
+            ushort expectedLength = 8;
+            bool expectedIsFixedValue = true;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(expectedLength, ae.Length);
+            Assert.AreEqual(expectedIsFixedValue, ae.IsFixedValue);
+        }
+
+        [TestMethod()]
+        [DataRow("19900423")]
+        [DataRow("20210404")]
+        [DataRow("20231219")]
+        public void 正規文字列の場合(string input)
+        {
+            // Arrange
+            var byteArray = Encoding.ASCII.GetBytes(input);
+            var ae = new DateValue(new Tag(0x1234, 0xabcd), byteArray);
+
+            // Act
+            var result = ae.GetValueObject();
+
+            // Assert
+            Assert.AreEqual(input, result);
+        }
+
+        [TestMethod()]
+        [DataRow("199a0423")]
+        [DataRow("202144")]
+        [DataRow("2023 219")]
+        [ExpectedException(typeof(InvalidDICOMFormatException))]
+        public void 不正文字列の場合(string input)
+        {
+            // Arrange
+            var byteArray = Encoding.ASCII.GetBytes(input);
+            var ae = new DateValue(new Tag(0x1234, 0xabcd), byteArray);
+            // 不正な入力なので、↑のインスタンス生成時に例外が発生することを期待
+
+            // Act
+
+            // Assert
+        }
+    }
+}
