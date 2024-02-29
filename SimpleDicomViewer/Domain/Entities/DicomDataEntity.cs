035ab91aace49a32998611c8d256c389242ff44d
Puye123 <puye.xiongtai@gmail.com>
2024-02-29 11:13:33 +0900

    Add: [WIP] DICOMファイルからの読み込みAPIの作成

---
+++ SimpleDicomViewer/Domain/Entities/DicomDataEntity.cs
@@ -0,0 +1,16 @@
+﻿using SimpleDicomViewer.Domain.ValueObjects.VR;
+using System.Collections.Generic;
+
+namespace SimpleDicomViewer.Domain.Entities
+{
+    public class DicomDataEntity
+    {
+        // DICOMデータ要素のリスト
+        public List<ValueElement> Values { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public DicomDataEntity() { }
+    }
+}
---
+++ SimpleDicomViewerTests/Infrastructure/File/DicomDataFileIOTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleDicomViewer.Infrastructure.File.Tests
+{
+    [TestClass()]
+    public class DicomDataFileIOTests
+    {
+        [TestMethod()]
+        public void ReadTest()
+        {
+            DicomDataFileIO dicomDataFileIO = new DicomDataFileIO();
+
+            // テストデータはここからダウンロード
+            // https://www.jira-net.or.jp/dicom/dicom_data_01_03.html 
+            dicomDataFileIO.Read("Infrastructure\\File\\US_LEE_IR6.dcm");
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
