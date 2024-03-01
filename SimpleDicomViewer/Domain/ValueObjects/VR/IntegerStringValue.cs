1d2352f6bbf4cd2ea91078b854c9b79f0fbd1d04
Puye123 <puye.xiongtai@gmail.com>
2024-03-01 14:11:49 +0900

    Add: ISを表すクラスを新規作成

---
+++ SimpleDicomViewer/Domain/ValueObjects/VR/IntegerStringValue.cs
@@ -0,0 +1,47 @@
+﻿using SimpleDicomViewer.Domain.Exceptions;
+using System;
+using System.Globalization;
+
+namespace SimpleDicomViewer.Domain.ValueObjects.VR
+{
+    /// <summary>
+    /// IS(Integer String: 整数列) : string
+    /// </summary>
+    public class IntegerStringValue : ValueElement
+    {
+        public IntegerStringValue(Tag tag, byte[] value) : base(tag, value, length: 12, isFixedValue: false, valueType: typeof(string))
+        {
+        }
+
+        protected override bool IsValidValue(byte[] value)
+        {
+            // 値の長さチェック
+            if (value.Length > Length)
+            {
+                throw new InvalidDICOMFormatException($"設定可能な値は最大{Length}バイトです。");
+            }
+
+            try
+            {
+                string valueString = System.Text.Encoding.ASCII.GetString(value);
+                NumberStyles ns = NumberStyles.Integer;
+                IFormatProvider fmt = CultureInfo.InvariantCulture;
+                int val;
+                bool result = int.TryParse(valueString, ns, fmt, out val);
+                if (result)
+                {
+                    return true;
+                }
+                else
+                {
+                    throw new InvalidDICOMFormatException($"設定可能な値は整数を表す文字列です。設定しようとした値は{valueString}です。");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                return false;
+            }
+        }
+    }
+}
---
+++ SimpleDicomViewerTests/Domain/ValueObjects/VR/IntegerStringValueTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleDicomViewer.Domain.Exceptions;
+using System.Text;
+
+namespace SimpleDicomViewer.Domain.ValueObjects.VR.Tests
+{
+    [TestClass()]
+    public class IntegerStringValueTests
+    {
+        [TestMethod()]
+        public void 値の長さが正しく設定されているか()
+        {
+            // Arrange
+            var ae = new IntegerStringValue(new Tag(0x1234, 0xabcd), new byte[] { 0x30, 0x31, 0x32, 0x30, 0x31, 0x32, 0x30, 0x31 });
+            ushort expectedLength = 12;
+            bool expectedIsFixedValue = false;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(expectedLength, ae.Length);
+            Assert.AreEqual(expectedIsFixedValue, ae.IsFixedValue);
+        }
+
+        [TestMethod()]
+        [DataRow("+2147483647")]
+        [DataRow("-2147483648")]
+        [DataRow("+123")]
+        [DataRow("-123")]
+        [DataRow("0")]
+        [DataRow("+0")]
+        [DataRow("-0")]
+        public void 正規文字列の場合(string input)
+        {
+            // Arrange
+            var byteArray = Encoding.ASCII.GetBytes(input);
+            var ae = new IntegerStringValue(new Tag(0x1234, 0xabcd), byteArray);
+
+            // Act
+            var result = ae.GetValueObject();
+
+            // Assert
+            Assert.AreEqual(input, result);
+        }
+
+        [TestMethod()]
+        [DataRow("12345678912345678")]
+        [DataRow("*123")]
+        [DataRow("12.1")]
+        [DataRow("-123f4")]
+        [DataRow("-123e-4")]
+        [DataRow("+2147483648")]
+        [DataRow("-2147483649")]
+        [ExpectedException(typeof(InvalidDICOMFormatException))]
+        public void 不正文字列の場合(string input)
+        {
+            // Arrange
+            var byteArray = Encoding.ASCII.GetBytes(input);
+            var ae = new IntegerStringValue(new Tag(0x1234, 0xabcd), byteArray);
+            // 不正な入力なので、↑のインスタンス生成時に例外が発生することを期待
+
+            // Act
+
+            // Assert
+        }
+    }
+}
