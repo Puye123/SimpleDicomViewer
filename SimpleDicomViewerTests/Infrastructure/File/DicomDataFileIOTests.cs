973f4ba2f575c76a3c4b925774a6f3683820d67c
Puye123 <puye.xiongtai@gmail.com>
2024-03-06 09:32:16 +0900

    ファイル読み込み処理の単体テストを修正

--- SimpleDicomViewerTests/Infrastructure/File/DicomDataFileIOTests.cs
+++ SimpleDicomViewerTests/Infrastructure/File/DicomDataFileIOTests.cs
@@ -8,8 +8,11 @@ namespace SimpleDicomViewer.Infrastructure.File.Tests
         [TestMethod()]
         public void ReadTest()
         {
+            // Arrange
             DicomDataFileIO dicomDataFileIO = new DicomDataFileIO();
+            int expectedElementCount = 38; // 別のビュワーアプリで確認した期待値
 
+            // Act
             // テストデータはここからダウンロード
             // https://www.jira-net.or.jp/dicom/dicom_data_01_03.html 
             var dicomData = dicomDataFileIO.Read("Infrastructure\\File\\US_LEE_IR6.dcm");
@@ -18,13 +21,8 @@ namespace SimpleDicomViewer.Infrastructure.File.Tests
                 Console.WriteLine($"{ve.Tag.ToString()} : {ve.GetType()} : {ve.GetValueObject().ToString()}");
             }
 
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void WriteTest()
-        {
-            Assert.Fail();
+            // Assert
+            Assert.AreEqual(expectedElementCount, dicomData.Values.Count);
         }
     }
 }
