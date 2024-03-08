e389d971329a9240bcc8f3b4048aa2293a9da0bf
Puye123 <puye.xiongtai@gmail.com>
2024-03-08 13:19:48 +0900

    Add: FilePickerサービスクラスを新規作成。DialogMessageサービスクラスのインターフェイスを修正

---
+++ SimpleDicomViewer/CommonServices/IFilePickerService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace SimpleDicomViewer.CommonServices
+{
+    public interface IFilePickerService
+    {
+        public Task<string?> FilePickAsync();
+
+        public Task<string?> FolderPickAsync();
+    }
+}
