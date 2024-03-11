c40b5383e51daefd8655c4b29de7681fa78ac672
Puye123 <puye.xiongtai@gmail.com>
2024-03-11 13:58:36 +0900

    不要なusingの削除

--- SimpleDicomViewer/ViewModels/TagListElement.cs
+++ SimpleDicomViewer/ViewModels/TagListElement.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using SimpleDicomViewer.Domain.ValueObjects.VR;
 
 namespace SimpleDicomViewer.ViewModels
